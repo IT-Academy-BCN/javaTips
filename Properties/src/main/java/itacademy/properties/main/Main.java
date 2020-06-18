759810739f34c71c2c5e41037d9ed749ea87e9dd
Jonatan Vicente <it@jonatanvicente.com>
2020-06-18 11:06:07 +0200

    Adding Properties example

---
+++ Properties/src/main/java/itacademy/properties/main/Main.java
@@ -0,0 +1,53 @@
+package itacademy.properties.main;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Set;
+
+public class Main {
+
+
+    Properties myProps;
+
+    public static void main (String ar[]){
+
+        Main m = new Main();
+        m.printSystemProperties();
+        m.loadProperties();
+
+
+    }
+
+    void loadProperties(){
+        InputStream input = getClass().getClassLoader().getResourceAsStream("application.properties");
+
+        myProps = new Properties();
+
+        System.out.println("******" + input == null);
+
+
+        try {
+            if (input !=null){
+                myProps.load(input);
+            }
+            System.out.println(myProps.getProperty("db.url"));
+            System.out.println(myProps.getProperty("db.user"));
+            System.out.println(myProps.getProperty("db.password"));
+
+        }catch (IOException ioe){
+            System.out.println("File not located " + ioe.getMessage());
+        }
+
+    }
+
+    void printSystemProperties(){
+        Properties systemProperties = System.getProperties();
+        systemProperties.forEach( (key, value) -> System.out.println(key.toString() + ": " + value.toString()));
+
+        System.out.println("Line separator: **" + System.lineSeparator() + "**");
+        System.out.println("Now: "+ System.currentTimeMillis());
+        System.out.println("Environment: " + System.getenv());
+    }
+}
--- README.md
+++ README.md
@@ -3,7 +3,30 @@
 
 Proyecto modular Gradle con algunos tips y demos ;)
 
+#### Ejecución de los ejemplos
 
-### Callback Pattern
+Ejecución global de todos los módulos (por línea de comandos o a través del IDE que utilices): invocación de la task <b>run</b> de Gradle. 
+- Linux / macOS
+``` 
+ ./gradlew run
+```
+- Windows
+```
+gradlew.bat run
+```
+
+Ejecución de cada módulo:
+
+- Linux / macOS
+``` 
+ ./gradlew :[nombreModulo]:run
+```
+- Windows
+```
+gradlew.bat :[nombreModulo]:run
+```
+
+
+#### Callback Pattern
 
 Breve demo de patrón callback
