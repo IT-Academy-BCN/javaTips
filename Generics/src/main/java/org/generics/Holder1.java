e32c698c9353fead783a7c52481cf870dff9b75d
Jonatan Vicente <it@jonatanvicente.com>
2020-11-05 09:46:14 +0100

    Adding Generics example

---
+++ Generics/src/main/java/org/generics/Holder1.java
@@ -0,0 +1,17 @@
+
+package org.generics;
+
+class Automobile {}
+
+public class Holder1 {
+
+  private Automobile a;
+
+  public Holder1(Automobile a) {
+    this.a = a;
+  }
+  Automobile get() {
+    return a;
+  }
+}
+
